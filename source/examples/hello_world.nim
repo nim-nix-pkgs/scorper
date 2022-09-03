546647c0e2bb97dcc45925b0a9c8aa52a8e550e0
hugosenari <hugosenari@gmail.com>
2022-09-03 19:57:40 -0300

    feat(nimpkgs): update lock

--- source/examples/hello_world.nim
+++
@@ -1,9 +0,0 @@
-import scorper
-
-const port{.intdefine.} = 8888
-when isMainModule:
-  proc cb(req: Request) {.async.} =
-    let headers = {"Content-type": "text/plain"}
-    await req.resp("Hello, World!", headers.newHttpHeaders())
-  const address = "127.0.0.1:" & $port
-  waitFor serve(address, cb)
--- source/src/scorper/http/rxnim/rxnim.nim
+++
@@ -1,4 +0,0 @@
-import ./ types
-import ./ observer
-import ./ observable
-export types, observer, observable
